4f6a1b56cfd5a70615cf3f87d5e80bf7d72985de
Aliaakib <aliaakibbukhari110@gmail.com>
2025-10-11 16:31:23 +0530

    fix: ensure redirects file for Netlify SPA routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import tailwindcss from '@tailwindcss/vite';
 
+
 export default defineConfig({
-  plugins: [tailwindcss()],
-   esbuild: {
-  logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  plugins: [react(),tailwindcss()],
+
+  build: {
+    outDir: "dist",
+  },
+  publicDir: "public",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
   },
 });
+
+
+
